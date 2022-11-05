755b0d928f233ab172d37118b684cc0a2c83ba76
Marah Ashayer <110784795+Marah-Ashayer@users.noreply.github.com>
2022-11-05 10:48:07 +0200

    Add files via upload

---
+++ Java Spring/Hopper's Receipt/hopperproject/src/main/java/com/codingdojo/hopper/sproject/HopperprojectApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.hopper.sproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopperprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopperprojectApplication.class, args);
+	}
+
+}
---
+++ Java Spring/Hopper's Receipt/hopperproject/src/test/java/com/codingdojo/hopper/sproject/HopperprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.hopper.sproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopperprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
