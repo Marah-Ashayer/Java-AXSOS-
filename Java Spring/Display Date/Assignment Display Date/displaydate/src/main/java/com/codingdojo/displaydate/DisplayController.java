e56c5ce3844b77105518c63d51df384caa6ed054
Marah Ashayer <110784795+Marah-Ashayer@users.noreply.github.com>
2022-11-05 10:50:05 +0200

    Add files via upload

---
+++ Java Spring/Display Date/Assignment Display Date/displaydate/src/main/java/com/codingdojo/displaydate/DisplayController.java
@@ -0,0 +1,37 @@
+package com.codingdojo.displaydate;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DisplayController {
+    @RequestMapping("/")
+    public String index() {
+        return "index.jsp";
+    }
+    
+    @RequestMapping("/time")
+    public String time(Model model) {
+    	String timePattern = "hh:mm a";
+    	SimpleDateFormat timeFormat = new SimpleDateFormat(timePattern);
+    	String time = timeFormat.format(new Date());	
+    	model.addAttribute("time", time);
+        return "time.jsp";
+    }
+    
+    @RequestMapping("/date")
+    public String date(Model model) {
+		String datePattern = "EEEEE, 'the' dd 'of' MMMMM, yyyy";
+		SimpleDateFormat dateFormat = new SimpleDateFormat(datePattern);
+		String date = dateFormat.format(new Date());	
+		model.addAttribute("date", date);
+        return "date.jsp";
+    }
+
+}
+
+
